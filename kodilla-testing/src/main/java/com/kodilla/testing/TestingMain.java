5bf3caf7b5778b42ad7ea878f5cf986dd5b661a8
Krzysztof Nosal <https://github.com/krzysiekn111/Krzysztof-Nosal-kodilla-java>
2022-03-12 22:13:14 +0100

    6.2 - test kalkulatora

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+import org.testng.annotations.Test;
+//import org.junit.jupiter.api.Assertions;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+//       kalkulator
+        int addingResult = Calculator.add(2,4);
+        double subtractingResult = Calculator.subtract(6,7);
+
+        if (addingResult != 2+4 || subtractingResult != 6-7) {
+            System.out.println("Kalkulator działa nieprawidłowo");
+        } else {
+            System.out.println("Kalkualtor działa prawidłowo");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,8 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public static int add(int a, int b) {
+        return a+b;
+    }
+    public static double subtract(int a, int b) {return a-b; }
+}
