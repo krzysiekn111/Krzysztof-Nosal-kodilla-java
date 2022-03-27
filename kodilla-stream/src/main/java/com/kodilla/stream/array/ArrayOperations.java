9d33dc58634f08ff310c6374248f412fba9715f6
Krzysztof Nosal <https://github.com/krzysiekn111/Krzysztof-Nosal-kodilla-java>
2022-03-27 11:07:58 +0200

    6.6. Pisanie testów JUnit i Mockito

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,22 +1,16 @@
 package com.kodilla.stream.array;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.OptionalDouble;
-import java.util.function.IntUnaryOperator;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
-    static OptionalDouble getAverage(List<Integer> numbers) {
-        OptionalDouble result = IntStream.range(0, numbers.size())
-                .map(operand -> numbers.get(operand))
-                .average();
-        System.out.println(result);
-
-        IntStream.range(0, numbers.size())
-                .map(operand -> numbers.get(operand))
+    static double getAverage(Integer[] numbers) {
+        IntStream.range(0, numbers.length)
+                .map(operand -> numbers[operand])
                 .forEach(System.out::println);
 
-        return result;
+        return IntStream.range(0, numbers.length)
+                .map(operand -> numbers[operand])
+                .average()
+                .orElse(0);
     }
 }
