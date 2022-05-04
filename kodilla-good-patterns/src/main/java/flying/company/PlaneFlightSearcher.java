d08bc7c8d0e9aaebd3b30ffd98abf84cc4d05040
Krzysztof Nosal <https://github.com/krzysiekn111/Krzysztof-Nosal-kodilla-java>
2022-05-04 17:50:58 +0200

    13.5. Przełożenie teorii na praktykę cz. 2

--- kodilla-good-patterns/src/main/java/flying/company/PlaneFlightSearcher.java
+++ kodilla-good-patterns/src/main/java/flying/company/PlaneFlightSearcher.java
@@ -35,7 +35,7 @@ public class PlaneFlightSearcher {
             }
         }
 
-        return flyingDirections;
+        return result;
     }
 
     public List<FlyingDirections> flightTo(String to) {
