0c99d5e696e94c808f7b34e7aa98394bcf7beac3
Krzysztof Nosal <https://github.com/krzysiekn111/Krzysztof-Nosal-kodilla-java>
2022-05-14 10:28:20 +0200

    14.3. Konfiguracja przy pomocy klasy Java

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -1,11 +1,14 @@
 package com.kodilla.spring.portfolio;
 
 public class Board {
-    private TaskList toDoList = new TaskList();
-    private TaskList doneList = new TaskList();
-    private TaskList inProgressList = new TaskList();
+    private TaskList toDoList;
+    private TaskList doneList;
+    private TaskList inProgressList;
 
     public Board(TaskList toDoList, TaskList doneList, TaskList inProgressList) {
+        this.toDoList = toDoList;
+        this.doneList = doneList;
+        this.inProgressList = inProgressList;
     }
 
 
--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -5,6 +5,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ContextConfiguration
@@ -19,13 +22,19 @@ public class BoardTestSuite {
         String inProgressTaskExample = "inProgressTaskExample";
 
         //when
-        board.getDoneList().getTasks().add(doneTaskExample);
-        board.getToDoList().getTasks().add(toDoTaskExample);
-        board.getInProgressList().getTasks().add(inProgressTaskExample);
 
         //then
+        board.getDoneList().getTasks().add(doneTaskExample);
         assertEquals(doneTaskExample, board.getDoneList().getTasks().get(0));
+        board.getDoneList().getTasks().remove(doneTaskExample);
+
+        board.getToDoList().getTasks().add(toDoTaskExample);
         assertEquals(toDoTaskExample, board.getToDoList().getTasks().get(0));
+        board.getToDoList().getTasks().remove(toDoTaskExample);
+
+
+        board.getInProgressList().getTasks().add(inProgressTaskExample);
         assertEquals(inProgressTaskExample, board.getInProgressList().getTasks().get(0));
+        board.getInProgressList().getTasks().remove(inProgressTaskExample);
     }
 }
