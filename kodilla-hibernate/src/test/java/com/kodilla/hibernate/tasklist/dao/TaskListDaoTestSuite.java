d947c432e8af10202d7e14ff70c5c9a92ca5bb8a
krzysiekn111 <krzysiekn111@gmail.com>
2022-06-12 21:25:36 +0200

    17.2. Tworzymy encje

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -1,16 +1,32 @@
-//package com.kodilla.hibernate.tasklist.dao;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//public class TaskListDaoTestSuite {
-//    @Autowired
-//    TaskListDao taskListDao;
-//
-//    @Test
-//    void testFindByListName() {
-//
-//    }
-//}
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+    @Autowired
+    TaskListDao taskListDao;
+
+    @Test
+    void testFindByListName() {
+        //Given
+        String listName = "list";
+        TaskList taskList = new TaskList(listName, "");
+        //When
+        taskListDao.save(taskList);
+        //Then
+        List<TaskList> listExample = taskListDao.findByListName(listName);
+        assertEquals(1, listExample.size());
+
+        //CleanUp
+        int id = taskList.getId();
+        taskListDao.deleteById(id);
+    }
+}
