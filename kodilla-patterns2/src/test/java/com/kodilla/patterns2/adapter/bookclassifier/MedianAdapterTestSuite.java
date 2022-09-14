5874e77794eda377ca23acd24ff8718593443cda
krzysiekn111 <krzysiekn111@gmail.com>
2022-09-14 12:44:01 +0200

    24.5

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -0,0 +1,10 @@
+package com.kodilla.patterns2.adapter.bookclassifier;
+
+import org.junit.jupiter.api.Test;
+
+public class MedianAdapterTestSuite {
+    @Test
+    void publicationYearMedianTest(){
+
+    }
+}
