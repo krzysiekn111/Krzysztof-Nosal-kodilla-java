2128d23bf3c9322ea80ded726e30983ea1eaae29
krzysiekn111 <krzysiekn111@gmail.com>
2022-10-03 10:42:20 +0200

    30.3. Procedury i funkcje – informacje uzupełniające

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,60 @@
+package com.kodilla.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StoredProcTestSuite {
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        // Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL='Not set'";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL='Not set'";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+
+        // When
+        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        statement.execute(sqlProcedureCall);
+
+        // Then
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(0, howMany);
+        rs.close();
+        statement.close();
+    }
+
+    @Test
+    void testUpdateBestsellers() throws SQLException {
+        // Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=0";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        // When
+        String sqlProcedureCall = "CALL UpdateBestSellers()";
+        statement.execute(sqlProcedureCall);
+
+        // Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=1";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int bestsellers = 0;
+        if (rs.next()) {
+            bestsellers = rs.getInt("HOW_MANY");
+        }
+        assertEquals(2, bestsellers);
+        rs.close();
+        statement.close();
+    }
+}
